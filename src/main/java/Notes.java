4e70117a2adbae975d5b081d3c1e3287a625ce2a
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-02-18 19:27:53 +0000

    update notes

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -2,42 +2,43 @@
 class Notes {
 	public static void main(String[] args) {
 
-		//Else if
+		//else if
 
 		int num;
-		num = 11;
+		num = 9;
 
-		if (num > 10) { //num > 10
+		if (num > 10) { // num > 10
 			System.out.println("if");
 		}
-		else if (num > 8) {  //num > 8 && num <= 10
-			System.out.println("else if");
+		else if (num > 8) { // num <= 10 && num > 8
+			System.out.println("else if 1");
 		}
-
-		else { //num <= 8
-			//num <= 10 && num <= 8
+		else if (num > 4) { // num <= 10 && num <= 8 && num > 4
+			//num <= 8 && num > 4
+			System.out.println("else if 2");
+		}
+		else { // nun <= 10  && num <= 8 && num <= 4
+			//num <= 4
 			System.out.println("else");
 		}
-		
 
 		//
 
 		if (num > 10) { //num > 10
 			System.out.println("if");
 		}
-		else if (num > 12) { //num > 12 && num <= 10
+		else if (num > 12) { //num <= 10 && num > 12
 			//UNREACHABLE ELSE IF STATEMENT
 			System.out.println("else if");
 		}
 			//could have more as many else ifs as i need
-		else { //num <= 10 && num <= 12
+		else {
 			System.out.println("else");
 		}
 
-		//Try to order your ifs and else ifs smaller sets of data to larger ones
-
-
-		//nested ifs - putting an if inside an if
+		//Try to order your ifs, else ifs in the order of smallest set of data to largest set of data
+	
+		//nested ifs - putting an if inside another if
 
 		if (num > 8) {
 			System.out.println("first if statement ran");
@@ -52,11 +53,9 @@ class Notes {
 			}
 		}
 
-		//repeated condition - nest it
+		//repeated condition - nest each of them
 		//Step by step checks
-		//code can be written between conditions
-
-		//Avoid over nested - makes code unreadable
+		//code between the conditions
 
 		// if () {
 		// 	if () {
@@ -72,9 +71,8 @@ class Notes {
 		// 	}
 		// }
 
-		//Break into functions
-		//Guard clauses can be used to avoid nested ifs. look this up if you want to use it
-
-		
+		//break into functions
+		//combine conditions
+		//Guard clauses can be used to avoid nested ifs.  Look it up!
 	}
 }
