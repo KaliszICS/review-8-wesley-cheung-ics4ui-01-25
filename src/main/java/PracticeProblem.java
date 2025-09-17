7f725e96043bc0651b7e6a58195d2911047f9daa
wesley-cheung-ics4ui-01-25 <166552362+wesley-cheung-ics4ui-01-25@users.noreply.github.com>
2025-09-17 17:48:29 +0000

    update practiceproblems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,8 +4,58 @@ public class PracticeProblem {
 
 	}
 
+	public static String pluralize(String input) {
+		input = input.toLowerCase();
+		int length = input.length();
+		if (length < 1) {
+			return "s";
+		} else if (length < 2) {
+			if (input.substring(0).equals("y")) {
+				return "ies";
+
+			} else {
+				return "s";
+			}
+			
+		} else if (length < 3) {
+			if (input.substring(0).equals("ey")) {
+				return "eys";
+
+			} else if (input.substring(1).equals("y")) {
+				return "ies";
+			} else {
+				return "s";
+			}
+		} else {
+			if (input.substring(length-2).equals("ey")) {
+				return "eys";
+			} else if (input.substring(length-1).equals("y")) {
+				return "ies";
+			} else if (input.substring(length-3).equals("ife")) {
+				return "ives";
+			} else {
+				return "s";
+			}
+		}
+		
+	}
 	public static int min(int num1, int num2, int num3) {
-		return num1;
+		return(Math.min(Math.min(num1,num2),num3));
+	}
+	public static boolean isLeapYear(int num1) {
+		if (num1 % 4 == 0) {
+			if (num1 % 100 == 0) {
+				if (num1 % 400 == 0) {
+					return true;
+				}	else {
+					return false;
+				}
+			}	else {
+				return true;
+			}
+		}	else {
+			return false;
+		}
 	}
 
 }
